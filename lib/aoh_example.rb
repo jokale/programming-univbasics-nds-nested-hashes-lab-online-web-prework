d2236161551a9782e47914580255013d0eab4d52
Joanna E K <kalemajoanna@gmail.com>
2019-11-05 10:56:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -28,7 +28,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   aoh [1][:name] = "Joelle van Dyne"
-  aoh [1]
+  aoh [1][:name]
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
